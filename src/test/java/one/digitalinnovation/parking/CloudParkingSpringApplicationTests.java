f41bb3d6e4c8353b33ae4ca5fd7ae44670b40fae
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-10-07 08:14:53 -0300

    project structure

---
+++ src/test/java/one/digitalinnovation/parking/CloudParkingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
